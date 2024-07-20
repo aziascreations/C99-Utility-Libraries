3f24d7057f891e1f3c4f6359c64a125da86df61f
Herwin Bozet <herwin.bozet@gmail.com>
2024-07-20 11:52:44 +0200

    Added `.def` files, Updated MSVC build script Update .gitignore, build-msvc.cmd, and 6 more files...

--- build-msvc.cmd
+++ build-msvc.cmd
@@ -10,11 +10,14 @@ set BUILD_DIR=%~dp0\build-msvc
 set VCVARS_ROOT=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 
 set FLAGS_CC=/nologo /Ot /Gy /Oy /TC /GS- /Gs9999999 /MD
-set FLAGS_LINK=/nologo /MANIFEST:EMBED
+set FLAGS_LINK=/nologo /emittoolversioninfo:no /emitpogophaseinfo /MANIFEST:EMBED
 
 set FLAGS_MSVCRT_CC=%FLAGS_CC%
 set FLAGS_MSVCRT_LINK=%FLAGS_LINK%
 
+:: Notes
+:: /emittoolversioninfo:no => Undocumented, Removes Rich PE stuff
+:: /emitpogophaseinfo      => Undocumented, MAY remove debug data from .pdata section
 
 :: Stuff you don't have to touch
 set BENCHMARK_DIR=%~dp0\benchmarks
@@ -63,8 +66,8 @@ echo ========================================
 call build-msvc-internal.cmd "%VCVARS_ROOT%\vcvars64.bat" "%BUILD_DIR%\x64-ucrt" "%SRC_DIR%" ^
                              "%BENCHMARK_DIR%" "%EXAMPLES_DIR%" "%RSC_DIR%" ^
                              "%FLAGS_CC%" ^
-                             "%FLAGS_LINK%" ^
-                             "%FLAGS_LINK%"
+                             "%FLAGS_LINK% notelemetry.obj" ^
+                             "%FLAGS_LINK% notelemetry.obj"
 echo.
 
 :build-x64-msvcrt
@@ -74,8 +77,8 @@ echo ========================================
 call build-msvc-internal.cmd "%VCVARS_ROOT%\vcvars64.bat" "%BUILD_DIR%\x64-msvcrt" "%SRC_DIR%" ^
                              "%BENCHMARK_DIR%" "%EXAMPLES_DIR%" "%RSC_DIR%" ^
                              "%FLAGS_MSVCRT_CC% /external:W4 /external:I ""%MSVCRT_HEADER_DIR%""" ^
-                             "%FLAGS_MSVCRT_LINK% /ENTRY:DllMain /NODEFAULTLIB user32.lib kernel32.lib ""%MSVCRT_LIB_DIR%\x64\win11-23h2-pro_msdn-iso\System32\msvcrt.lib""" ^
-                             "%FLAGS_MSVCRT_LINK% /ENTRY:main /NODEFAULTLIB user32.lib kernel32.lib ""%MSVCRT_LIB_DIR%\x64\win11-23h2-pro_msdn-iso\System32\msvcrt.lib"""
+                             "%FLAGS_MSVCRT_LINK% /ENTRY:DllMain /NODEFAULTLIB notelemetry.obj user32.lib kernel32.lib ""%MSVCRT_LIB_DIR%\x64\win11-23h2-pro_msdn-iso\System32\msvcrt.lib""" ^
+                             "%FLAGS_MSVCRT_LINK% /ENTRY:main /NODEFAULTLIB notelemetry.obj user32.lib kernel32.lib ""%MSVCRT_LIB_DIR%\x64\win11-23h2-pro_msdn-iso\System32\msvcrt.lib"""
 echo.
 
 :build-x86-ucrt
@@ -84,21 +87,22 @@ echo ========================================
 call build-msvc-internal.cmd "%VCVARS_ROOT%\vcvars32.bat" "%BUILD_DIR%\x86-ucrt" "%SRC_DIR%" ^
                              "%BENCHMARK_DIR%" "%EXAMPLES_DIR%" "%RSC_DIR%" ^
                              "%FLAGS_CC%" ^
-                             "%FLAGS_LINK%" ^
-                             "%FLAGS_LINK%"
+                             "%FLAGS_LINK% notelemetry.obj" ^
+                             "%FLAGS_LINK% notelemetry.obj"
 echo.
 
-:build-x86-msvcrt
-echo C99 Goodies - x86 MSVCRT Build
-echo ========================================
-call build-msvc-internal.cmd "%VCVARS_ROOT%\vcvars32.bat" "%BUILD_DIR%\x86-msvcrt" "%SRC_DIR%" ^
-                             "%BENCHMARK_DIR%" "%EXAMPLES_DIR%" "%RSC_DIR%" ^
-                             "%FLAGS_MSVCRT_CC% /external:W4 /external:I ""%MSVCRT_HEADER_DIR%""" ^
-                             "%FLAGS_MSVCRT_LINK% /ENTRY:DllMain /NODEFAULTLIB user32.lib kernel32.lib ""%MSVCRT_LIB_DIR%\x86\win11-23h2-pro_msdn-iso_x64\SysWOW64\msvcrt.lib""" ^
-                             "%FLAGS_MSVCRT_LINK% /ENTRY:main /NODEFAULTLIB user32.lib kernel32.lib ""%MSVCRT_LIB_DIR%\x86\win11-23h2-pro_msdn-iso_x64\SysWOW64\msvcrt.lib"""
-:: https://yal.cc/cpp-a-very-tiny-dll/
-:: ___allmul => _allmul => ntdll.lib
-echo.
+:: I can't figure out the __allmul issue right now, I'll handle it later
+:::build-x86-msvcrt
+::echo C99 Goodies - x86 MSVCRT Build
+::echo ========================================
+::call build-msvc-internal.cmd "%VCVARS_ROOT%\vcvars32.bat" "%BUILD_DIR%\x86-msvcrt" "%SRC_DIR%" ^
+::                             "%BENCHMARK_DIR%" "%EXAMPLES_DIR%" "%RSC_DIR%" ^
+::                             "%FLAGS_MSVCRT_CC% /external:W4 /external:I ""%MSVCRT_HEADER_DIR%""" ^
+::                             "%FLAGS_MSVCRT_LINK% /ENTRY:DllMain /ALTERNATENAME:__allmul=_allmul /NODEFAULTLIB notelemetry.obj user32.lib kernel32.lib ""%MSVCRT_LIB_DIR%\x86\win11-23h2-pro_msdn-iso_x64\SysWOW64\msvcrt.lib""" ^
+::                             "%FLAGS_MSVCRT_LINK% /ENTRY:main /ALTERNATENAME:__allmul=_allmul /NODEFAULTLIB notelemetry.obj user32.lib kernel32.lib ""%MSVCRT_LIB_DIR%\x86\win11-23h2-pro_msdn-iso_x64\SysWOW64\ntdll.lib"" ""%MSVCRT_LIB_DIR%\x86\win11-23h2-pro_msdn-iso_x64\SysWOW64\msvcrt.lib"""
+:::: _ftlused => https://yal.cc/cpp-a-very-tiny-dll/
+:::: ___allmul => _allmul => ntdll.lib => Could that work: https://github.com/microsoft/STL/issues/2655
+::echo.
 
 
 :: Letting the user check the logs
